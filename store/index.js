2b8de4773a0c89cd567ec051b6ceae0767f3c8a7
Amr2812 <ana.osama.elmohamady@gmail.com>
2021-05-04 10:06:39 -0700

    fix: set isAuth to false on nuxtServerInit

--- store/index.js
+++ store/index.js
@@ -2,6 +2,8 @@ export const actions = {
   nuxtServerInit({ commit }, { req }) {
     if (req.headers.cookie && req.headers.cookie.includes("connect.sid=")) {
       commit("auth/isAuthenticated", true);
+    } else {
+      commit("auth/isAuthenticated", false);
     }
   }
 };

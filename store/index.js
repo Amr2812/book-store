477712ce4413e76bcf1c332932853461c7e018d6
Amr2812 <ana.osama.elmohamady@gmail.com>
2021-05-04 10:00:38 -0700

    fix: check if headers includes cookies

--- store/index.js
+++ store/index.js
@@ -1,6 +1,6 @@
 export const actions = {
   nuxtServerInit({ commit }, { req }) {
-    if (req.headers.cookie.includes("connect.sid=")) {
+    if (req.headers.cookie && req.headers.cookie.includes("connect.sid=")) {
       commit("auth/isAuthenticated", true);
     }
   }

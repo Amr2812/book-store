6fed1cb8156777b959c50756fe4abec9b03d3478
Amr2812 <ana.osama.elmohamady@gmail.com>
2021-05-04 23:51:26 -0700

    fix(vuex-persist): store cart module only

--- plugins/vuex-persist.js
+++ plugins/vuex-persist.js
@@ -3,6 +3,7 @@ import VuexPersistence from "vuex-persist";
 export default ({ store }) => {
   new VuexPersistence({
     key: "vuex",
-    storage: window.localStorage
+    storage: window.localStorage,
+    modules: ["cart"]
   }).plugin(store);
 };
